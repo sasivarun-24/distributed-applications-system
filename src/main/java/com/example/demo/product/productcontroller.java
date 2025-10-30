2a0c1c6e68e3c687cc8e12760301693dc876f3b0
Sasi Varun Ramachandran <sasivarun2424@gmail.com>
2025-10-30 09:58:31 +0100

    bonus

--- src/main/java/com/example/demo/product/productcontroller.java
+++ src/main/java/com/example/demo/product/productcontroller.java
@@ -1,8 +1,13 @@
 package com.example.demo.product;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 public class productcontroller {
@@ -19,4 +24,23 @@ public class productcontroller {
     public List<Product> getAllProducts() {
         return products;
     }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable int id) {
+        Optional<Product> product = products.stream().filter(a->a.getId()==id).findFirst();;
+        if (product.isPresent()) {
+            return ResponseEntity.ok(product.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/products/filter")
+    public List<Product> getProductByFilter(@RequestParam(required = false) String name, @RequestParam(required = false) String size) {
+        return  products.stream()
+                .filter(p -> (name == null || p.getName().equalsIgnoreCase(name))
+                        && (size == null || p.getSize().equalsIgnoreCase(size)))
+                .collect(Collectors.toList());
+    }
+
 }
